93237badb747093d6ab328ac8fce4b899c64523a
liangshuai0222 <liangshuai0222@126.com>
2016-06-02 18:40:56 +0800

    zengjia

--- src/guardSuspensionn/TestThread.java
+++ src/guardSuspensionn/TestThread.java
@@ -5,8 +5,6 @@ public class TestThread {
 		RequestQueue requestQueue = new RequestQueue();
 		for(int i=0;i<5;i++) new ServerThread(requestQueue, "ServerThread "+i).start();
 		for(int i=0;i<5;i++) new ClientThread(requestQueue, "CliendThred "+i).start();
-		System.out.println("合并+");
-		System.out.println("冲突");
-		System.out.println("增加");
+		System.out.println("hello");
 	}
 }
