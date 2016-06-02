40665eb7e371392aaff76e1dfb9bd9c1e94d7e3c
liangshuai0222 <liangshuai0222@126.com>
2016-06-02 14:11:28 +0800

    并发设计模式 保护暂停

---
+++ src/guardSuspensionn/TestThread.java
@@ -0,0 +1,9 @@
+package guardSuspensionn;
+
+public class TestThread {
+	public static void main(String[] args) {
+		RequestQueue requestQueue = new RequestQueue();
+		for(int i=0;i<5;i++) new ServerThread(requestQueue, "ServerThread "+i).start();
+		for(int i=0;i<5;i++) new ClientThread(requestQueue, "CliendThred "+i).start();
+	}
+}
