d411ceba6eff1f3cb528a57b4d512cf10be458f5
liangshuai0222 <liangshuai0222@126.com>
2016-06-02 17:45:00 +0800

    update 冲突

--- src/guardSuspensionn/TestThread.java
+++ src/guardSuspensionn/TestThread.java
@@ -6,5 +6,6 @@ public class TestThread {
 		for(int i=0;i<5;i++) new ServerThread(requestQueue, "ServerThread "+i).start();
 		for(int i=0;i<5;i++) new ClientThread(requestQueue, "CliendThred "+i).start();
 		System.out.println("合并");
+		System.out.println("冲突");
 	}
 }
